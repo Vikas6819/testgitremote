7d7de00086c6646f2e552c11fcef00fecb894364
vikas <dhongadev70@gmail.com>
2020-08-01 19:28:26 +0530

    2nd commit

--- src/mypack/TestGitDemo.java
+++ src/mypack/TestGitDemo.java
@@ -5,6 +5,7 @@ public class TestGitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Connected With Git Repo Successfully");
+		System.out.println("Added new line");
 	}
 
 }
